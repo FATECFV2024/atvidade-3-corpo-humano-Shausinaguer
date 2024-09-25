7d82ce7150388e00509b12eb0609134773898dfc
Shausinaguer <149312021+Shausinaguer@users.noreply.github.com>
2024-09-25 10:29:23 -0300

    Update CorpoHumano.java
    Adicionado parte do desenvolvimento da Classe "CorpoHumano".

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,23 @@
 package main.java;
 
 public class CorpoHumano {
+    // metodo_acesso - tipagem - nome
+    private double massa;
+    private double altura;
 
+    public CorpoHumano(double massa, double altura){
+        this.massa = massa;
+        this.altura = altura;
+    }
+
+    //Setter do corpo humano
+    public void setCorpo (double ent_massa, double ent_altura){
+        massa = ent_massa;
+        altura = ent_altura;
+    }
+
+    //Getter do corpo humano
+    public double getCorpo(){
+        return massa;
+    }
 }
