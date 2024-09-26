447738ee87132d8f5820f876b7a7f8258930893d
Shausinaguer <149312021+Shausinaguer@users.noreply.github.com>
2024-09-26 11:10:33 -0300

    (Update) Atualização de alguns erros

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,29 +4,24 @@ import java.util.Scanner;
 public class App {
         //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
+    public static void main(String[] args) throws Exception{
+        try (//Biblioteca para entrada de dados
+        Scanner ler = new Scanner(System.in)) {
+            //variável local a
+            double app_massa;
+            double app_altura;
 
-        //variável local a
-        double app_massa;
-        double app_altura;
+            System.out.println("Comando de Saida de Dados");
+            
+            System.out.printf("Informe seu peso: ");
+            app_massa = ler.nextDouble();
 
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe seu peso: ");
-        app_massa = ler.nextDouble();
+            System.out.printf("Informe sua altura: ");
+            app_altura = ler.nextDouble();
 
-        System.out.printf("Informe sua altura: ");
-        app_altura = ler.nextDouble();
+            CorpoHumano imc = new CorpoHumano(app_massa, app_altura);
 
-        CorpoHumano imc = new CorpoHumano(app_massa, app_altura);
-
-        System.out.println(imc.getmassa() / imc.getaltura() );
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
-
+            System.out.println(imc.getmassa() / imc.getaltura() );
+        }
     }
 }
